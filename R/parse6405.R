6a108027b53f38ef752e6288ac46cd9240aaf29c
Brady Allred <brady.allred@okstate.edu>
2012-04-07 23:21:34 -0500

    use regular expressions for parsing 6405 files in parse6405()

--- R/parse6405.R
+++ R/parse6405.R
@@ -26,55 +26,52 @@ parse6405 <- function(call, yearmonth, force=F) {
   lines6405 <- readLines(file6405, warn=F)
   
   ## remove brackets
-  lines6405 <- gsub("\\[", " ", lines6405)
-  lines6405 <- gsub("]", " ", lines6405)
-  ## split into list at whitespaces; treat multiple whitespaces as one
-  list6405 <- strsplit(lines6405, " +")
-  ## convert each list element to a data frame
-  list6405 <- lapply(list6405, function(x) {x <- as.data.frame(t(x), 
-                                                               stringsAsFactors=F)
-                                            colnames(x) <- 1:length(x)
-                                            x[x=="M"] <- NA
-                                            return(x)})
-  ##records with columns < 8 set as NA
-#   list6405 <- lapply(list6405,function(x) {if(length(x)<8) return(NA)
-#                                            else return(x)})
+  #lines6405 <- gsub("\\[", " ", lines6405)
+  #lines6405 <- gsub("]", " ", lines6405)
   
-  ## runway visual regular expression
-  vis.pattern <- "(^\\d{1,2}?[A-Z])|(\\+$)"
+  ## use regular expression to extract wind data
+  wind.pattern  <- paste(" ",
+                         "((M[[:space:]]{1,8})|([0-9]{1,3}[[:space:]]+))",
+                         "((M[[:space:]]{1,8})|([0-9]{1,3}[[:space:]]+))",
+                         "((M[[:space:]]+)|([0-9]{1,3}[[:space:]]+))",
+                         "((M[[:space:]]+)|([0-9]{1,3}[[:space:]]{1}))", 
+                         sep="")
+  
+  wind <- lapply(lines6405, function(x) {
+    windexpr <- regexpr(text=x, pattern=wind.pattern)
+    if(windexpr>0) {
+      wind.vec <- unlist(strsplit(substr(x, start=windexpr+1, 
+                                         stop=windexpr+attr(windexpr, 
+                                                            "match.length")),
+                                  " +"))
+      ## replace Ms with NA
+      wind.vec[wind.vec=="M"] <- NA
+      return(wind.vec)
+      #if(length(wind.vec)==4) return(wind.vec) else return(c(NA, NA, NA, NA))
+    } else c(NA, NA, NA, NA)
+  })
   
   ## two minute wind direction
-  D360 <- sapply(list6405, function(x) { if(length(x)>=8) {
-    if(any(grepl(x, pattern=vis.pattern))) last <- length(x)-1
-    else last <- length(x)
-    return(as.integer(x[, last-3]))
-  } else return(NA)})
+  D360 <- sapply(wind, function(x) as.integer(x[1]))
   ## two minute wind speed (knots)
-  M2SP <- sapply(list6405, function(x) { if(length(x)>=8) {
-    if(any(grepl(x, pattern=vis.pattern))) last <- length(x)-1
-    else last <- length(x)
-    return(as.integer(x[, last-2]))
-  } else return(NA)})
+  M2SP <- sapply(wind, function(x) as.integer(x[2]))
   ## direction of maximum (5 second average) wind during previous minute
-  MXD5 <- sapply(list6405, function(x) { if(length(x)>=8) {
-    if(any(grepl(x, pattern=vis.pattern))) last <- length(x)-1
-    else last <- length(x)
-    return(as.integer(x[, last-1]))
-  } else return(NA)})
+  MXD5 <- sapply(wind, function(x) as.integer(x[3]))
   ## speed of maximum (5 second average) wind during previous mintue (knots)
-  MXS5 <- sapply(list6405, function(x) { if(length(x)>=8) {
-    if(any(grepl(x, pattern=vis.pattern))) last <- length(x)-1
-    else last <- length(x)
-    return(as.integer(x[, last]))
-  } else return(NA)})
-  ## time
-  TIME <- sapply(list6405, function(x) {substr(x[, 2], 4, 15)})
-  ## get UTC offset from asosstns
+  MXS5 <- sapply(wind, function(x) as.integer(x[4]))
+  
+  ## use regular expression to extract time
+  time.pattern <- " [A-Z]{3}\\d+"
+  ## utc offset
   utcoffset <- asosstns$UTC[match(substr(call, 2, 4), asosstns$CALL)]
   ## use sprintf() to convert to character, "-0000" or "+0000"
   utcoffset <- sprintf("%03+d", utcoffset)
+  
+  timeexpr <- regexpr(text=lines6405, pattern=time.pattern)
+  TIME <- substr(lines6405, start=timeexpr+4, stop=timeexpr+15)
   TIME <- as.POSIXct(paste(TIME, utcoffset, "00", sep=""), 
                      format="%Y%m%d%H%M%z")
-  ## return as data frame
+  
+ ## return as data frame
   return(data.frame(CALL=call, TIME, D360, M2SP, MXD5, MXS5))
 }
